c68581e18a67d1e1f3a61f07f4ebbb50a0106854
“Aria <a.c.nikzad@gmail.com>
2019-12-10 19:30:55 -0800

    First Vue app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Add product name here:<input type="text" v-model="newProductName">
+    <p>Add product description here<input type="text" v-model="newProductDescription">
+    <p>Add product price here<input type="text" v-model="newProductPrice">
+    <p>Add product image url here<input type="text" v-model="newProductImageUrl">
+
+    <button v-on:click="addProduct()">Create Product</button>
+
+
+    <div v-for="product in products">
+      <p>{{product.name}}</p>
+      <p>{{product.description}}</p>
+
+    <br>
+
+    <img v-bind:src="product.image_url">
+
+    <br>
+
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: "",
+    };
+  },
+  created: function() {
+    console.log('hello');
+    axios.get("/api/products").then(response => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
+  methods: {
+    addProduct: function() {
+
+      var params = {
+        name: this.newProductName,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image_url: this.newProductImageUrl
+      };
+
+      axios.post("/api/products", params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.newProductName = "";
+        this.newProductDescription = "";
+        this.newProductPrice = "";
+        this.newProductImageUrl = "";
+      });
+    }
   }
-}
+};
 </script>
