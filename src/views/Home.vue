26ee4857edc953b60907c4d3e6667da7150863f7
“Aria <a.c.nikzad@gmail.com>
2019-12-11 18:24:39 -0800

    finished restful actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,28 @@
 
     <button v-on:click="addProduct()">Create Product</button>
 
-
     <div v-for="product in products">
-      <p>{{product.name}}</p>
-      <p>{{product.description}}</p>
+      <p>Name: {{product.name}}</p>
+      <p>Description: {{product.description}}</p>
 
-    <br>
+      <br>
 
-    <img v-bind:src="product.image_url">
+      <img v-bind:src="product.image_url">
+      
+      <br>
 
-    <br>
+      <button v-on:click="toggleInfo(product)">See more info</button>
+   
+      <div v-if="currentProduct === product">
+        <p> price: {{product.price}}</p>
 
+        <p>Name: <input type="text" v-model="product.name"></p>
+        <p>Description: <input type="text" v-model="product.description"></p>
+        <p>Price: <input type="text" v-model="product.price"></p>
+        <p>Image URL: <input type="text" v-model="product.image_url"></p>
+        <button v-on:click="updateProduct(product)">Update this product</button>
+        <button v-on:click="destroyProduct(product)">Delete this product</button>
+      </div>
     </div>
   </div>
 </template>
@@ -37,6 +48,7 @@ export default {
       newProductDescription: "",
       newProductPrice: "",
       newProductImageUrl: "",
+      currentProduct: {},
     };
   },
   created: function() {
@@ -63,6 +75,46 @@ export default {
         this.newProductDescription = "";
         this.newProductPrice = "";
         this.newProductImageUrl = "";
+      });
+    },
+    toggleInfo: function(theProduct) {
+      console.log(theProduct);
+      if (this.currentProduct === theProduct) {
+        this.currentProduct = null;
+      } else {
+        this.currentProduct = theProduct;
+      }
+      console.log('in toggle info...');
+    },
+
+    updateProduct: function(theProduct) {
+      console.log("updating the product");
+      console.log(theProduct);
+
+      var params = {
+        name: theProduct.name,
+        description: theProduct.description,
+        price: theProduct.price,
+        imageUrl: theProduct.image_url,
+      };
+
+      axios.patch(`/api/products/$[theProduct.id}`, params).then(response => {
+        console.log(response.data);
+        theProduct.name = response.data.name;
+        theProduct.description = response.data.description;
+        theProduct.price = response.data.price;
+        theProduct.imageUrl = response.data.imageUrl;
+      });
+    },
+
+    destroyProduct: function(theProduct) {
+      console.log(theProduct);
+      axios.delete(`/api/products${theProduct.id}`).then(response => {
+        console.log(response.data);
+
+        var index = this.products.indexOf(theProduct);
+        console.log(index);
+        this.products.splice(index, 1);
       });
     }
   }
